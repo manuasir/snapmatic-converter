fd07655d97e1815fd5bce2b577d4b3c4c73680ed
Manuel J. Bernal <manu.asi2@gmail.com>
2019-05-01 23:34:22 +0200

    Changes in global module.

--- bin/snapmatic-converter.js
+++ bin/snapmatic-converter.js
@@ -1,12 +1,15 @@
 const snapmaticToJpeg = require('../lib/snapmatic-to-jpeg');
 const { debug } = require('../package.json');
 // start process if correct number of arguments was passed
-if (process.argv.length <= 3) {
-  throw new Error('Usage:  snapmatic-converter <directory>');
+if (process.argv.length != 4) {
+  console.log(
+    'Usage:  snapmatic-converter <source folder> <destination folder>'
+  );
+  process.exit(-1);
 }
 const src = process.argv[2];
 const dst = process.argv[3];
 debug && console.log('src ', src);
 debug && console.log('dst ', dst);
 const converter = new snapmaticToJpeg(src, dst);
-converter.process();
+converter.convertAllFiles();
